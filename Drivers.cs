26f10fc710d92155ec279042cbc955a68dc25b8d
tiwatee <kriswendie4eva@gmail.com>
2022-08-04 07:44:00 +0100

    first commit

---
+++ Drivers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwoMiDa
+{
+    internal class Drivers
+    {
+        public string VehicleType()
+        {
+            Console.WriteLine("Do you drive a keke, okada, korope or big bus? ");
+            string vehicle = Console.ReadLine();
+            string vvehicle = vehicle.ToLower();
+            switch (vvehicle)
+            {
+                case "keke":
+                    return "50";
+
+                case "okada":
+                    return "30";
+                    
+                case "korope":
+                    return "100";
+
+                case "big bus":
+                    return "200";
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    return "0";
+            }
+        }
+
+        public double Membership()
+        {
+            string ms = "invalid";
+            while((ms != "0") || (ms != "1"))
+            {
+                Console.WriteLine("If you are a member of the NURTW, type 1. If not, type 0: ");
+                ms = Console.ReadLine();
+                if ((ms == "0")||(ms=="1"))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+            if (ms == "0")
+            {
+                double rate = 1;
+                return rate;
+            }
+            else if (ms == "1")
+            {
+                double rate = 0.7;
+                return rate;
+
+            }
+            else
+            {
+                return 5.0;
+            }
+
+
+        }
+
+        public double TollFee(double rate, int vehicleTypeCharge)
+        {
+            Console.WriteLine("How many trips did you take today? ");
+            int noOfTrips=Convert.ToInt32(Console.ReadLine());
+
+            double tollFee = Convert.ToInt32(vehicleTypeCharge) * noOfTrips * rate;
+            return tollFee;            
+        }
+
+        public double TotalAmount()
+        {
+            double Amount = 0;
+            for (int i=1; i<=5; i++)
+            {
+                Console.WriteLine("Driver " + i + ":");
+                double rate = Membership();
+                int vehicleTypeCharge = Convert.ToInt32(VehicleType());
+                Amount += TollFee(rate,vehicleTypeCharge);
+            }
+            return Amount;
+        }      
+   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OwoMiDa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drivers amount = new Drivers();
+            Console.WriteLine("The total toll for the five drivers is: "+amount.TotalAmount());
+        }
+    }
+}
